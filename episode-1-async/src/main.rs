5d477bc63ac705ba19a2b82b56cd83fde4075d46
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-06-15 11:56:52 -0300

    Finished async examples

--- episode-1-async/src/main.rs
+++ episode-1-async/src/main.rs
@@ -1,15 +1,14 @@
 mod executor;
 mod task;
 
-use std::process::exit;
-
 use executor::Executor;
+use futures::pending;
 use task::Task;
 
-// new!
 async fn brush_teeth(times: usize) -> String {
     for i in 0..times {
         println!("Brushing teeth {}", i);
+        pending!(); // new!
     }
     return "Done".to_string();
 }
@@ -17,15 +16,16 @@ async fn brush_teeth(times: usize) -> String {
 fn main() {
     let mut executor = Executor::new();
 
-    let future = brush_teeth(10); // new!
+    let future = brush_teeth(10);
     let task1 = Task::new("first_task".to_string(), future);
+
     executor.tasks.push(task1);
 
     while executor.tasks.len() != 0 {
         for task in executor.tasks.iter_mut() {
             task.poll();
         }
-        println!("Went through all tasks once"); // new!
+        println!("Went through all tasks once");
 
         // clean up tasks that are done
         executor.tasks.retain(|task| !task.is_done());
